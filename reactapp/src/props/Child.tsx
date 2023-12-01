0c3a3d291d1b1815df179ea24a0a2711456ef24a
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 00:44:43 +0100

    Added clickevent parameter

--- reactapp/src/props/Child.tsx
+++ reactapp/src/props/Child.tsx
@@ -1,9 +1,15 @@
 interface ChildProps {
     color: string;
+    clickEvent: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-    return <div>{color}</div>;
+export const Child = ({ color, clickEvent }: ChildProps) => {
+    return (
+        <div>
+            {color}
+            <button onClick={clickEvent}>Click Me</button>
+        </div>
+    );
 }
 
 // specify that Child is a Function Component, allowing it to use default properties given with FE's
