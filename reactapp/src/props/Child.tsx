886a98e5ad93a93d0a4cb5b64238ba88b13312bf
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 00:30:50 +0100

    added FE variant to Child

--- reactapp/src/props/Child.tsx
+++ reactapp/src/props/Child.tsx
@@ -5,3 +5,8 @@ interface ChildProps {
 export const Child = ({ color }: ChildProps) => {
     return <div>{color}</div>;
 }
+
+// specify that Child is a Function Component, allowing it to use default properties given with FE's
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+    return <div>{color}</div>;
+}
