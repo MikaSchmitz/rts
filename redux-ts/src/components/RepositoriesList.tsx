e6f0e5383376a6c0b2b481b56a5c01cfe55548ba
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 20:17:28 +0100

    added a submit form to repositoriesList

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const RepositoriesList = () => {
+    const [term, setTerm] = useState('');
+
+    const search = (event: React.FormEvent<HTMLFormElement>) => {
+        // prevent the page from refreshing on submit
+        event.preventDefault();
+
+
+    }
+
     return (
         <div>
-            <form>
-                <input />
+            <form onSubmit={search}>
+                <input value={term} onChange={(event) => setTerm(event.target.value)} />
                 <button>Search</button>
             </form>
         </div>
