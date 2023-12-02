72293aa86f5c19d87f0c8df9da9c0c376b9c3b40
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 20:30:30 +0100

    check if API call works

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import {actionCreators } from '../state'
 
 const RepositoriesList = () => {
     const [term, setTerm] = useState('');
+    const dispatch = useDispatch();
 
     const search = (event: React.FormEvent<HTMLFormElement>) => {
         // prevent the page from refreshing on submit
         event.preventDefault();
 
-
+        dispatch(actionCreators.searchRepositories(term) as any);
     }
 
     return (
