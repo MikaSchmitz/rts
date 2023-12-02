812a7dac418cdd4c9bab9089535640b50fd1c5ae
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 19:14:45 +0100

    Added base for action-creators searchRepositories

---
+++ redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action }  from "../actions";
+
+const searchRepositories = (term: string) => {
+    return async (dispatch: any) => {
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES
+        });
+
+        try {
+
+        } catch (err) {
+            if (!(err instanceof Error)){
+                return;
+            }
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                payload: err.message
+            });
+        }
+    }
+}
