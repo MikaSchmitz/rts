f5ad1307784d2e3cf50e1fbd3fbe86bffd28045e
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 20:43:49 +0100

    Select repositories (packages) after API call

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useSelector } from 'react-redux';
 import { useActions } from '../hooks/useActions'
 
 const RepositoriesList = () => {
     const [term, setTerm] = useState('');
     const { searchRepositories } = useActions();
+    const state = useSelector((state: any) => state.repositories);
 
     const search = (event: React.FormEvent<HTMLFormElement>) => {
         // prevent the page from refreshing on submit
