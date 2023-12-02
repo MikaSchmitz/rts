cfbd7a431ba41b6d864b5bebf500a1f1b148f349
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 11:56:11 +0100

    event tutorial

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,10 +1,11 @@
 import GuestList from './state/GuestList';
 import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
 
 const App = () => {
 	return (
 		<div>
-			<UserSearch />
+			<EventComponent />
 		</div>
 	);
 }
