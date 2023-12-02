792dde613363a56a918d19b640dc1ce3e682ee5a
Mika Schmitz <47559434+MikaSchmitz@users.noreply.github.com>
2023-12-02 11:34:22 +0100

    Moved index startup logic to index file

---
+++ reactapp/src/App.tsx
@@ -0,0 +1,12 @@
+import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
+
+const App = () => {
+	return (
+		<div>
+			<UserSearch />
+		</div>
+	);
+}
+
+export default App;
